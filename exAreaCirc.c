5be4437c49c5c781e14fc077a25461f506c1b0fb
Antonio Luiz Neto <antonioluiz.n.eng@gmail.com>
2023-03-30 22:11:00 -0300

    Update exAreaCirc.c

--- exAreaCirc.c
+++ exAreaCirc.c
@@ -1,28 +1,28 @@
 /*IMPLEMENTAR UMA FUNÇÃO PARA CALCULAR A AREA DE UM ANEL FORMADO POR UM CIRCULO MENOR DENTRO DE UM CIRCULO MAIOR*/
 #include <stdio.h>
+#include <math.h>
 #define pi 3.1415
+
 float area_anel(float raioMaior, float raioMenor)
 {
     int area;
-    area = ((pi * raioMaior * raioMaior)-(pi * raioMenor * raioMenor));
+    area = pi * (pow(raioMaior, 2)) - pi * (pow(raioMenor, 2));
     return area;
 }
 
 int main()
 {
-    float areaAnel, r1, r2;
-    // if(r1 > r2)
-        areaAnel = area_anel(r1, r2);
-    // else
-    //     areaAnel = area_anel(r2, r1);
-    
-    printf("Insira o raio do primeiro círculo: ");
-    scanf("%f", &r1);
+    float areaAnel=0, rMaior=0, rMenor=0;
 
-    printf("Insira o raio do segundo círculo: ");
-    scanf("%f", &r2);
+    printf("Insira o valor do circulo maior: ");
+    scanf("%f", &rMaior);
 
-    printf("A área do anel é %f", areaAnel);
+    printf("Insira o valor do circulo menor: ");
+    scanf("%f", &rMenor);
+
+    areaAnel = area_anel(rMaior, rMenor);
+
+    printf("O valor da area do anel formado por dois circulos de diferentes tamanhos sobrepostos e de %f", areaAnel);
+
+    return 0;
 }
-
-// ERRADO, CORRIGIR

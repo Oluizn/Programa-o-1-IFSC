4e25e0fe43da06960980d07e73c15fe3163f6f3e
Antonio Luiz Neto <104032875+Oluizn@users.noreply.github.com>
2023-04-28 16:50:28 -0300

    Add files via upload

---
+++ exProva.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int funcao(int x)
+{
+    int f;
+    if (x >= 10){
+        f = 5*(x*x) + 3;
+    }
+    else if (x > 5 && x < 10)
+    {
+        f = x;
+    }
+    else
+        f = -3*(x*x) -1;
+    
+    return f;
+}
+
+int funcao_menor(int maior, int menor)
+{
+    int i, f, j;
+
+    j = funcao(menor);
+
+    for (i = menor; i <= maior; i++)
+    {
+        f = funcao(i);
+
+        if (f < j)
+            j = f;
+    }
+    return j;
+    
+}
+int main()
+{
+    printf("%d\n", funcao(2));
+    printf("%d\n", funcao_menor(15, 0));
+}

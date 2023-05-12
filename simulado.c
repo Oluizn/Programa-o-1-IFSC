5f99538f79c22c4ae4ad82cd1285e63159d0b101
Antonio Luiz Neto <104032875+Oluizn@users.noreply.github.com>
2023-05-12 16:49:39 -0300

    Add files via upload

--- simulado.c
+++ simulado.c
@@ -5,7 +5,7 @@ int elementos_pares(int vet1[], int tam)
     int i, aux = 0;
 
     for (i = 0; i < tam; i++)
-    {        
+    {
         if (vet1[i]%2 == 0)
             aux++;
     }
@@ -14,7 +14,7 @@ int elementos_pares(int vet1[], int tam)
 
 int compare_vetores(int x[], int y[], int tam)
 {
-    int i, aux1, aux2, result;
+    int aux1, aux2, result;
     aux1 = elementos_pares(x, tam);
     aux2 = elementos_pares(y, tam);
     

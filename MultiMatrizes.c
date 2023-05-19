29bfd1db3c66eb89583b0e69a3cd22c6f907ddd3
Antonio Luiz Neto <antonioluiz.n.eng@gmail.com>
2023-05-19 13:38:46 -0300

    klbchj

--- MultiMatrizes.c
+++ MultiMatrizes.c
@@ -11,7 +11,7 @@ int mult_matriz(int mat1[2][2], int mat2[2][3], int matR[2][3])
         matR[0][i]=mat1[0][0]*mat2[0][i]+mat1[0][1]*mat2[1][i];
             for (j = 0; j < 3; j++)
             {
-            matR[1][j]=mat1[1][0]*mat2[0][j]+mat1[1][0]*mat2[1][j];
+                matR[1][j]=mat1[1][0]*mat2[0][j]+mat1[1][0]*mat2[1][j];
             }
     }
     
@@ -21,14 +21,13 @@ int print_tabela(int tabela[2][3])
     int i;
     for (i = 0; i < 3; i++)
     {
-        printf("%d,\n", &tabela[i][0]);
+        printf("%d,\n", &tabela[i][i]);
     }
 }
 int main()
 {
     int matA[2][2]={{3,2},{5,-1}}, matB[2][3]={{6,5,-2},{0,7,1}}, matR[2][3];
     mult_matriz(matA, matB, matR);
-    printf("%d", &matR[0][0]);
-    
+    print_tabela(matR);
     return 0;
 }

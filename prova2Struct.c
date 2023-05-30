ef6369b6b37bd2da3dd91c8f3d7f6c3c97914bc6
Antonio Luiz Neto <antonioluiz.n.eng@gmail.com>
2023-05-30 18:52:43 -0300

    study

--- prova2Struct.c
+++ prova2Struct.c
@@ -5,23 +5,37 @@ struct acesso
 {
     char nome[10], senha[10], direito[3]; 
 };
-int procurar_usuario(struct acesso x, char usuario[])
+int search_userID(struct acesso x[3], char userID[10])
+{
+    for (int i = 0; i < 3; i++)
+        if (strcmp(x[i].nome, userID)==0)
+            return i+1;
+    return -1;
+}
+void change_password(struct acesso x[3], char userID[10],char newpassword[10])
+{
+    int aux;
+    aux=search_userID(x->nome, userID);
+    if(aux!=-1)
+        strcpy(x[aux].senha, newpassword);
+}
+
+void print_tabela(struct acesso x[3])
 {
     for (int i = 0; i < 3; i++)
     {
-        if(strcmp(&x[i].nome, usuario)==0);
+        printf("%s\t", x[i].nome);
+        printf("%s\t", x[i].senha);
+        printf("%s\n", x[i].direito);
     }
-    return -1;
 }
-
 int main()
 {
     struct acesso tabelaUsuario[3] = {{"eraldo", "beta", "rw"},{"silvana", "alfa", "r"},{"vica", "delta", "w"}};
-    for (int i = 0; i < 3; i++)
-    {
-        printf("%s\n", tabelaUsuario[i].nome);
-        printf("%s\n", tabelaUsuario[i].senha);
-        printf("%s\n", tabelaUsuario[i].direito);
-    }
+    printf("%d\n", search_userID(tabelaUsuario, "eraldo"));
+    print_tabela(tabelaUsuario);
+    change_password(tabelaUsuario, "eraldo", "omega");
+    print_tabela(tabelaUsuario);
+    
     return 0;
 }

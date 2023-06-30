600657e26bd1fcb9db6689b75e53120b9bdf36ea
Antonio Luiz Neto <104032875+Oluizn@users.noreply.github.com>
2023-06-30 16:50:06 -0300

    exercicio corrigido

--- ex1prova3.c
+++ ex1prova3.c
@@ -31,22 +31,39 @@ tipo_aluno *retorna_aluno(tipo_aluno *pturma, char *nome)
 float media_aluno(tipo_aluno *pturma, char *nome)
 {
     float media=0;
-    int x[3];
-    retorna_aluno(pturma, nome);
-    for (int i = 0; i < 3; i++)
-    {
-        x[i]=&pturma[i].aval;
+    tipo_aluno *aluno=retorna_aluno(pturma, nome);
+    if (aluno!=NULL){
+        for (int i = 0; i < 3; i++)
+            media+=aluno->aval[i];    
+        return media/3;
     }
+    else
+        return -1;
 }
-float media_turma(tipo_aluno *turma)
+float media_turma(tipo_aluno *pturma)
 {
-
+    float soma=0;
+    for (int i = 0; i < TAM_USERS; i++)
+    {
+        soma+=media_aluno(pturma, pturma[i].nome);
+    }
+    return soma/TAM_USERS; 
 }
 int numero_alunos_acima_media(tipo_aluno *pturma)
 {
-
+    float cont=0, media_turma_valor = media_turma(pturma);
+    for (int i = 0; i < TAM_USERS; i++)
+        if (media_aluno(pturma, pturma[i].nome) > media_turma_valor)
+            cont++;
+    return cont;
 }
 int main()
 {
     printf("%s\n", retorna_aluno(tabela_alunos, "Silvana e Silva"));
+    if (retorna_aluno(tabela_alunos, "Antonio")==NULL)
+        printf("Usuário não encontrado na tabela\n");
+    printf("media do aluno %s = %.2f\n",tabela_alunos->nome, media_aluno(tabela_alunos, "Eraldo e Silva"));
+    printf("%.2f\n", media_aluno(tabela_alunos, "Luiz"));
+    printf("media da turma %.2f\n", media_turma(tabela_alunos));
+    printf("%d aluno acima da média\n", numero_alunos_acima_media(tabela_alunos));
 }

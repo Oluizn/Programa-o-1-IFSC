cd83855f13603366ce1c7f6ffd65887fc98966c7
Antonio Luiz Neto <antonioluiz.n.eng@gmail.com>
2023-07-04 15:11:09 -0300

    att

--- prova/ex2rec3.c
+++ prova/ex2rec3.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int num_caracteres(char *p, char *letra)
+int num_caracteres(char *p, char letra)
 {
     int cont=0;
     while (*p!=0){
-        if(*p==*letra)
+        if(*p==letra)
             cont++;
         p++;
     }
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
     for (int i = 1, j=2; i < argc; i=i+2, j=j+2){
         printf("%s\t", argv[i]);
         printf("(%s)", argv[j]);
-        printf("%d\t", num_caracteres(argv[i], argv[j]));
+        printf("%d\t", num_caracteres(argv[i], *argv[j]));
     }
     return 0;
 }
